789c22ac0f84ce8c3a8dbfe13925b9accec738e1
Gregor <174007895+gloffler@users.noreply.github.com>
2025-05-29 15:58:52 +0200

    feat: add function to return random selection

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    if (randomNumber <= 0.3) {
+        return "Rock";
+    } else if (randomNumber >= 0.6) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
