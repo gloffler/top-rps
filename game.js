15060411a8279db8409e2cc71ec2f50779d6ac47
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-10 23:02:01 +0200

    refractor format code with prettier

--- game.js
+++ game.js
@@ -1,65 +1,65 @@
 function getComputerChoice() {
-    let randomNumber = Math.random();
-    if (randomNumber <= 0.3) {
-        return "rock";
-    } else if (randomNumber >= 0.6) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  let randomNumber = Math.random();
+  if (randomNumber <= 0.3) {
+    return "rock";
+  } else if (randomNumber >= 0.6) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
 function getHumanChoice() {
-    let choice = prompt("Your choice:", "").toLowerCase();
-    switch (choice) {
-        case "rock":
-        case "paper":
-        case "scissors": 
-            break;
-        default:
-            getHumanChoice();
-    }
-    return choice
+  let choice = prompt("Your choice:", "").toLowerCase();
+  switch (choice) {
+    case "rock":
+    case "paper":
+    case "scissors":
+      break;
+    default:
+      getHumanChoice();
+  }
+  return choice;
 }
 
 function playRound(humanChoice, computerChoice) {
-    const rules = {
-        rock: "scissors",
-        paper: "rock",
-        scissors: "paper"
-    };
+  const rules = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
 
-    if (humanChoice === computerChoice) {
-        return "draw";
-    }
+  if (humanChoice === computerChoice) {
+    return "draw";
+  }
 
-    return rules[humanChoice] === computerChoice ? "human" : "computer";
+  return rules[humanChoice] === computerChoice ? "human" : "computer";
 }
 
 function playGame() {
-    const rounds = 5;
-    let humanScore = 0;
-    let computerScore = 0;
-    let message;
-    
-    for (let i = 0; i < rounds; i++) {
-        let roundWinner = playRound(getHumanChoice(), getComputerChoice());
-        if (roundWinner == "computer") {
-            computerScore++;
-        } else if (roundWinner == "human") {
-            humanScore++;
-        }
+  const rounds = 5;
+  let humanScore = 0;
+  let computerScore = 0;
+  let message;
+
+  for (let i = 0; i < rounds; i++) {
+    let roundWinner = playRound(getHumanChoice(), getComputerChoice());
+    if (roundWinner == "computer") {
+      computerScore++;
+    } else if (roundWinner == "human") {
+      humanScore++;
     }
-    
-    if (humanScore === computerScore) {
-        message = "Draw!";
-    } else if (humanScore > computerScore) {
-        message = "Human wins!";
-    } else {
-        message = "Computer wins!";
-    }
-    
-    alert(message);
+  }
+
+  if (humanScore === computerScore) {
+    message = "Draw!";
+  } else if (humanScore > computerScore) {
+    message = "Human wins!";
+  } else {
+    message = "Computer wins!";
+  }
+
+  alert(message);
 }
 
 playGame();
